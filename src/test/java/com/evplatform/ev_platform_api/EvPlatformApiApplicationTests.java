289e211c9a8a6c2990e98bd61af9ae1fb2550a42
irtem <emre_irtem@hotmail.com>
2025-07-01 20:29:41 +0300

    initial commit

---
+++ src/test/java/com/evplatform/ev_platform_api/EvPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evplatform.ev_platform_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvPlatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
